36def07628b6270ad091c9b84d3980efebfd212b
umutbektass <umutbektass.78@hotmail.com>
2023-08-05 13:14:52 +0300

    rooting, paging and component structure created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { ProjectPageComponent } from './pages/project-page/project-page.component';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
 
 const routes: Routes = [
   {path:'',component:HomePageComponent},
    {path:'project',component:ProjectPageComponent},
+   {path:'about',component:AboutPageComponent},
   // {path:'2',component:Page2Component}
 
 ];
